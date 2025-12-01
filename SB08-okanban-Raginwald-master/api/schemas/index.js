6ee5b96fd2a78df1db43e62ee24b48198675cc3b
AndreolleManuel <manuel.andreolle@proton.me>
2025-12-01 10:19:04 +0100

    Delete SB08-okanban-Raginwald-master directory

--- SB08-okanban-Raginwald-master/api/schemas/index.js
+++
@@ -1,3 +0,0 @@
-export { createListSchema, updateListSchema } from "./list.schemas.js";
-export { createCardSchema, updateCardSchema } from "./card.schemas.js";
-export { createTagSchema, updateTagSchema } from "./tag.schemas.js";
